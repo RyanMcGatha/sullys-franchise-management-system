6b76b1557808ea96d7d176b3ee5e244040da2b52
r.mcgatha <ryanmcgatha@gmail.com>
2024-03-12 14:44:47 -0400

    added OTP auth, added profile page with file storage for profile pic which updates on click to supabase db

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,7 @@
-import { useState } from "react";
-import { supabase } from "./supabaseClient";
+import Routes from "./routes/Routes";
 
-import "./App.css";
+function App() {
+  return <Routes />;
+}
 
-const SignUpForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-
-  async function handleSignUp() {
-    const { data, error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: "https:/localhost:5173",
-      },
-    });
-  }
-
-  return (
-    <div>
-      <h1>Sign Up</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSignUp();
-        }}
-      >
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            name="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Sign Up</button>
-      </form>
-      {error && <div>{error}</div>}
-    </div>
-  );
-};
-
-export default SignUpForm;
+export default App;
---
+++ frontend/src/pages/ProtectedLayout.jsx
@@ -0,0 +1,10 @@
+const ProtectedLayout = () => {
+  return (
+    <div>
+      <h1>Protected Layout</h1>
+      <p>This is a protected layout.</p>
+    </div>
+  );
+};
+
+export default ProtectedLayout;
