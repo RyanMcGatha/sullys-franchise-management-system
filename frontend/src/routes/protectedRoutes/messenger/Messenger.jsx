a686b9b5b6194044e179d1e54f20dda5914381e9
r.mcgatha <ryanmcgatha@gmail.com>
2024-03-15 14:01:11 -0400

    added realtime coms with supa base for messaging system

--- frontend/src/routes/protectedRoutes/messenger/Messenger.jsx
+++ frontend/src/routes/protectedRoutes/messenger/Messenger.jsx
@@ -1,9 +1,42 @@
 import "./messenger.css";
+import { supabase } from "../../../config/supabaseConfig";
+import { useState } from "react";
+import React from "react";
 
 const Messenger = () => {
+  const [message, setMessage] = useState("");
+
+  const main = supabase.channel("main");
+
+  const handleSendMessage = async (event) => {
+    event.preventDefault();
+
+    main.send({
+      type: "broadcast",
+      event: "test",
+      payload: {
+        message: message,
+      },
+    });
+  };
+
   return (
     <div className="main-messenger">
       <h1>Messenger</h1>
+      <div className="chatBox"></div>
+      <form onSubmit={handleSendMessage} className="newMessageForm">
+        <input
+          className="message"
+          type="text"
+          placeholder="Enter your message"
+          value={message || ""}
+          required={true}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button className="sendMessageBtn" type="submit">
+          Send
+        </button>
+      </form>
     </div>
   );
 };
